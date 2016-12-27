3f4dd5e8aeb7eef69e4c464f24ef79781da6cf43
Chris Helgert <chris.helgert@online.de>
2016-12-27 20:17:11 +0100

    build-jobs for usage in other projects and npm-release

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+
+const env = process.env.NODE_ENV;
+
+const config = {
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+    ],
+  },
+  output: {
+    library: 'react-handler',
+    libraryTarget: 'umd',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
+  ],
+};
+
+if (env === 'production') {
+  const uglifyJS = new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      warnings: false,
+    },
+  });
+
+  config.plugins.push(uglifyJS);
+}
+
+module.exports = config;
